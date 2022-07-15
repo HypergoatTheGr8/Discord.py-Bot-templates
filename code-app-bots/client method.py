52671149de9f10b714cd77d6f9abd8c12fdc0cd1
HypergoatTheGr8 <103937187+HypergoatTheGr8@users.noreply.github.com>
2022-07-15 12:28:40 -0400

    Update client method.py

--- code-app-bots/client method.py
+++ code-app-bots/client method.py
@@ -1,1 +1,19 @@
+import discord
+import os
 
+#this is repl.it version, so make sure you have a secret with your token in it. you can name it whatever you want
+token = os.environ["token"]
+client = discord.Client()
+bot = "Bot_template#1234"
+
+@client.event
+async def on_ready():
+  print(f"{bot} is ready!")
+  
+@client.event
+async def on_message(message):
+  if message.content.startswith("+hello"):
+    await message.channel.send("hi there!")
+    
+  elif message.content.startswith("+quote"):
+    
